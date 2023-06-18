b21ffaf00d2ab52580638bca91478b7835859e1d
Paras <parasv249@gmail.com>
2023-06-18 12:00:19 +0200

    Add display pizza list

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+//Added Namespaces
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList {get;set;} = default!;
+
+        public PizzaListModel(PizzaService service)
+        {
+            _service = service;
+        }
+
         public void OnGet()
         {
+            PizzaList = _service.GetPizzas();
         }
     }
 }
